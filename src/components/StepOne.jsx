1d6c108b52f5ccc977d491f72865396dd763bb3b
Chinguun <centillionc0919@gmail.com>
2024-12-23 12:01:42 +0800

    Update StepOne.jsx

--- src/components/StepOne.jsx
+++ src/components/StepOne.jsx
@@ -1,14 +1,14 @@
-import { Focus } from "lucide-react";
+import classNames from 'classnames';
 
 
-export function StepOne({ SetCurrentStep, form, onChange, error, error2, isTrue}) {
+export function StepOne({ SetCurrentStep, form, onChange, error, isTrue, isTrue2, stepTwoIsTrue}) {
 
 
     
 
 
   const handleSubmit = () => {
-    if (isTrue) {
+    if (isTrue == true && isTrue2 == true) {
       SetCurrentStep(2);
     } else {
     }
@@ -20,21 +20,33 @@ export function StepOne({ SetCurrentStep, form, onChange, error, error2, isTrue}
     return (
         <div className="items-center justify-center flex h-screen w-screen bg-[#F4F4F4]">
         <div className="w-[480px] h-[655px] rounded-[8px] bg-white justify-center flex "> 
-            <div className=" w-[416px] h-[385px] mt-[32px] mx-[32px]"> 
+            <div className=" relative w-[416px] h-[385px] mt-[32px] mx-[32px]"> 
                 <img src="Main 1.svg"></img>
                 <h1 className="text-left text-[26px] font-bold ">Join Us! 😎</h1>
                 <h3 className="text-left text-[#8E8E8E] text-[18px] font-normal">Please provide all current information accurately.</h3>
                 <h1 className="text-[14px] mt-[28px] font-bold">First name</h1>
-                <input type="text" id="firstName" placeholder="First Name" value={form.firstName} onChange={onChange}  className=" w-[416px] h-[44px] bg-white border-[1px] border-[#0CA5E9] rounded-[12px] pl-[13px] "></input>
+                <input type="text" id="firstName" placeholder="First Name" value={form.firstName} onChange={onChange}  className={classNames(
+                          "w-[416px] h-[44px] bg-white border-[1px] rounded-[12px] pl-[13px]",
+                          {
+                            "border-[#0CA5E9]": form.firstName === "" && stepTwoIsTrue.firstName, 
+                            "border-[#E14942] text-[#E14942]": form.firstName !== "" && !stepTwoIsTrue.firstName, 
+                          }
+                        )}></input>
 
-               <p id='error' className="text-[#E14942] font-[400] text-[14px] ">{error}</p> 
+               <p id='error' className="text-[#E14942] font-[400] text-[14px] ">{error.firstName}</p> 
                 <h1 className="text-[14px] mt-[12px] font-bold">Last name</h1>
-                <input type="text" id="lastName"  onChange={onChange} value={form.lastName}   className=" w-[416px] h-[44px] bg-white border-[1px] border-[#CBD5E1] rounded-[12px] pl-[13px] "></input>
-                <p id='error' className="text-[#E14942] font-[400] text-[14px] ">{error2}</p> 
+                <input type="text" id="lastName"  onChange={onChange} value={form.lastName}   className={classNames(
+                          "w-[416px] h-[44px] bg-white border-[1px] rounded-[12px] pl-[13px]",
+                          {
+                            "border-[#0CA5E9]": form.lastName === "" && stepTwoIsTrue.lastName, 
+                            "border-[#E14942] text-[#E14942]": form.lastName !== "" && !stepTwoIsTrue.lastName, 
+                          }
+                        )}></input>
+                <p id='error' className="text-[#E14942] font-[400] text-[14px] ">{error.lastName}</p> 
                 <h1 className="text-[14px] mt-[12px] font-bold">User name</h1>
                 <input type="text" id="userName"  onChange={onChange} value={form.userName}    className=" w-[416px] h-[44px] bg-white border-[1px] border-[#CBD5E1] rounded-[12px] pl-[13px]"></input>
                 <button 
-        className="bg-[#D6D8DB] text-[#A9ACAF] h-[44px] w-[416px] rouned-[6px] px-4 mt-[162px] rounded hover:bg-gray-800" 
+        className="bg-[#D6D8DB] text-[#A9ACAF] h-[44px] w-[416px] rouned-[6px] px-4  rounded hover:bg-gray-800 absolute left-0 bottom-[-220px] " 
         onClick={handleSubmit}>
         Continue 1/3 {">"}
       </button>
