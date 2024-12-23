344a641c5afed81a0dcb6d32414714049ff04426
Chinguun19 <centillionc0919@gmail.com>
2024-12-23 11:58:35 +0800

    update

---
+++ src/components/StepFour.jsx
@@ -0,0 +1,14 @@
+export function Success ({ SetCurrentStep }) {
+    return (
+        <div className=" justify-center flex h-screen w-screen bg-[#F4F4F4]">
+        <div className="w-[480px] h-[193px] mt-[182px] rounded-[8px] bg-white justify-center flex  ">
+        <div className=" w-[416px] h-[385px] mt-[32px] mx-[32px]"> 
+        <img src="Main 1.svg" className="w-[60px] h-[60px]  "></img>
+                <h1 className="text-left text-[26px] font-[600]  ">You're All Set 🔥</h1>
+                <h3 className="text-left text-[#8E8E8E] text-[18px]  font-[400]">We have received your submission. Thank you!</h3>
+                </div>
+       </div>
+       </div>
+    );
+  }
+  
